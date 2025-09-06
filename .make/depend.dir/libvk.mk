cd80f473bd16b0105ea52ac57db2db1969a7e9e0
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-07 06:19:37 +0800

    Sun Sep  7 06:19:37 AM CST 2025

--- .make/depend.dir/libvk.mk
+++ .make/depend.dir/libvk.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libvk.recursive
 ECHO_TARGETS += libvk.echo
 libvk.recursive:
-	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libui/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libui/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/ && make recursive && make install
-	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/.make/super
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libui/               >> /home/lidali/alpine/libraries/libvk/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libui/.make >> /home/lidali/alpine/libraries/libvk/.make/super
+	cd /home/lidali/alpine/libraries/libvk/ && make recursive && make install
+	-rm /home/lidali/alpine/libraries/libvk/.make/super
 libvk.echo:
-	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libui/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libui/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/ && make echo
-	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libvk/.make/super
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libui/               >> /home/lidali/alpine/libraries/libvk/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libui/.make >> /home/lidali/alpine/libraries/libvk/.make/super
+	cd /home/lidali/alpine/libraries/libvk/ && make echo
+	-rm /home/lidali/alpine/libraries/libvk/.make/super
