a5ce36256b9d90f52042894c940ac2309dca08cd
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-07 06:34:45 +0800

    Sun Sep  7 06:34:45 AM CST 2025

--- src/core/UIApplication.cpp
+++ src/core/UIApplication.cpp
@@ -1,4 +1,5 @@
 #include "UIApplication.h"
+using namespace foxintango;
 
 UIApplication::UIApplication(){
 }
--- src/core/UIApplication.h
+++ src/core/UIApplication.h
@@ -2,11 +2,13 @@
 #define _UI_APPLICATION_H_
 #include <libcpp/libcpp.h>
 
-namespaceBegin(tango)
+EXTERN_C_BEGIN
+namespaceBegin(foxintango)
 class foxintangoAPI UIApplication{
 public:
     UIApplication();
     virtual ~UIApplication();
 };
 namespaceEnd
+EXTERN_C_END
 #endif
