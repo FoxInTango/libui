dbdcf89d12d76cb69beeb2d6d5aee0aef8fd2d2a
李诗军 <lidali@AllinoneBook.local>
2025-10-14 15:57:52 +0800

    2025年10月14日 星期二 15时57分52秒 CST

--- .make/depend.dir/libvk.mk
+++ .make/depend.dir/libvk.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libvk.recursive
 ECHO_TARGETS += libvk.echo
 libvk.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libui/               >> /volumes/llama/home/alpine/libraries/libvk/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libui/.make >> /volumes/llama/home/alpine/libraries/libvk/.make/super
-	cd /volumes/llama/home/alpine/libraries/libvk/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libvk/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libui/               >> /Users/lidali/alpine/libraries/libvk/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libui/.make >> /Users/lidali/alpine/libraries/libvk/.make/super
+	cd /Users/lidali/alpine/libraries/libvk/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libvk/.make/super
 libvk.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libui/               >> /volumes/llama/home/alpine/libraries/libvk/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libui/.make >> /volumes/llama/home/alpine/libraries/libvk/.make/super
-	cd /volumes/llama/home/alpine/libraries/libvk/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libvk/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libui/               >> /Users/lidali/alpine/libraries/libvk/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libui/.make >> /Users/lidali/alpine/libraries/libvk/.make/super
+	cd /Users/lidali/alpine/libraries/libvk/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libvk/.make/super
